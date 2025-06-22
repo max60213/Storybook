7f31d6a933e30ecf428f7e587c3e81d2c20375f1
Max Lin <max60213@gmail.com>
2025-06-22 15:16:59 +0800

    initialize again

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
-  // Turbopack configuration for SVG handling
-  turbopack: {
-    rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
-      },
-    },
-  },
 };
 
 export default nextConfig;
